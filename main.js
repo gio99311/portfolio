27f608753e80b1f0d7082b9d72d46c7eb83c970a
Giorgia Marcantonio <giorgia.marcantonio@outlook.it>
2026-01-07 12:39:21 +0100

    aggiunta la feature interattiva della navbar, migliorato l'header, inserita sezione skills provvisoria

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-// NAVBAR
+// NAVBAR INTERATTIVA
+const navbarLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('#home, #skills');
 
-let navbar = document.querySelector(".navbar");
+// Observer per scroll
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const sectionId = entry.target.getAttribute('id');
+            navbarLinks.forEach(link => {
+                link.classList.remove('cust-link');
+                if (link.getAttribute('href') === `#${sectionId}`) {
+                    link.classList.add('cust-link');
+                }
+            });
+        }
+    });
+}, { threshold: 0.5 });
 
-window.addEventListener("scroll", () => {
-    if(window.scrollY){
-        navbar.classList.add("scrolledNav");
-    } else {
-        navbar.classList.remove("scrolledNav");
-    }
-})
+sections.forEach(section => observer.observe(section));
+
--- readme.md
+++ readme.md
@@ -1,7 +1,12 @@
 ## Colors
-primary: #002642
-secondary: #E5DADA
-accent: #840032
+--color-p:  #17120b;
+    --color-s: #dcdcdc;
+    --color-a: #D6EFFF;
+    --color-b: #488286;
+
+#FF7D00
+#F44708
+#C73E1D
 
 ## Fonts
 Questrial
